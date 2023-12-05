b34ef5ef48a500471f3a4954c86f5c547f024922
Nagham Saleh <nagham.mzs02@gmail.com>
2023-12-05 22:03:27 +0300

    installl packages

---
+++ DiagnosisSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DiagnosisSystem.entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiagnosisSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<UsersClass> Users { get; set; }
+        public DbSet<RoleClass> Roles { get; set; }
+    }
+}
