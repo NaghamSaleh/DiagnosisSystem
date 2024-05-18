f68576486261899da185d415e092a6520735f448
NaghamSaleh <nagham.mzs02@gmail.com>
2024-05-18 20:37:24 +0300

    forum views

--- DiagnosisSystem/Migrations/20240518162650_updates.Designer.cs
+++ DiagnosisSystem/Migrations/20240518162650_updates.Designer.cs
@@ -4,6 +4,7 @@ using DiagnosisSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiagnosisSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240518162650_updates")]
+    partial class updates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DiagnosisSystem/Migrations/20240518162650_updates.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiagnosisSystem.Migrations
+{
+    public partial class updates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Queries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tag",
+                table: "Queries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+          
+        }
+    }
+}
